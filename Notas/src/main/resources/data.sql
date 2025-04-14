3c5ea9988b29a2575fdfc7543c18a75cc9628985
DaniellAngelo <dcsggg5@gmail.com>
2025-04-14 19:53:09 -0300

    melhorias e correções

---
+++ Notas/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO nota (aluno_id, disciplina_id, valor) VALUES
+                                                      (1, 1, 8.5),
+                                                      (1, 2, 7.0),
+                                                      (2, 1, 9.0),
+                                                      (3, 3, 6.5);
